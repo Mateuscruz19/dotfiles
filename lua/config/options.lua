8b0f9c9909c8c913f091f286df6c3c4eb0cfcc52
Mateus Cruz <mateus.cruz@sagemcom.com>
2026-03-23 19:17:10 -0300

    feat: add autosave on buffer leave and focus lost

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,9 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
+
+-- Autosave ao trocar de buffer ou perder foco
+vim.api.nvim_create_autocmd({"BufLeave", "FocusLost"}, {
+  pattern = "*",
+  command = "silent! wa",
+})
